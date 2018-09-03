56aa6eec94d6b477140fa94aa5d091fc0457d745
svedpathak <svedpathak@shutterfly.com>
2018-09-03 14:27:40 +0530

    Added label

--- src/pages/lobDetailsPage.js
+++ src/pages/lobDetailsPage.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Grid from '../components/grid';
 import Axios from 'axios';
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+import { Label, Loader} from 'semantic-ui-react'
 
 export default class LOBDetails extends Component{
     constructor (props) {
@@ -22,14 +22,14 @@ export default class LOBDetails extends Component{
     }
 
     loadData(){
-        // Axios.get(`http://localhost:8080/mockData/${this.state.table}.json`)
-        // .then(resp => {
-        //     this.setState({tableData:resp.data});
-        // });
-        Axios.get(`http://localhost:56564/api/${this.state.table}`)
+        Axios.get(`http://localhost:8080/mockData/${this.state.table}.json`)
         .then(resp => {
             this.setState({tableData:resp.data});
         });
+        // Axios.get(`http://localhost:56564/api/${this.state.table}`)
+        // .then(resp => {
+        //     this.setState({tableData:resp.data});
+        // });
     }
 
     render(){
@@ -42,7 +42,12 @@ export default class LOBDetails extends Component{
         }
         else {
             return(
-                <Grid database={this.state.database} table={this.state.table} tableData={this.state.tableData}/>
+                <div>
+                    <Label pointing='right' size='large'>{this.state.database}</Label>
+                    <Label pointing='below' size='large'>{this.state.table}</Label>
+                    <br/>
+                    <Grid database={this.state.database} table={this.state.table} tableData={this.state.tableData}/>
+                </div>
             );
         }
     }
